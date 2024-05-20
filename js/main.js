5af731d6965554548da919752273a249a9712cac
ArvinBagheriSani <arvinbs1382@gmail.com>
2024-05-20 23:32:50 +0330

    ver 0.1.1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.querySelector(".popularitem1").addEventListener("click", function () {
+  document.querySelector(".underpopular1").style.display = "block";
+  document.querySelector(".underpopular2").style.display = "none";
+});
+document.querySelector(".popularitem2").addEventListener("click", function () {
+  document.querySelector(".underpopular1").style.display = "none";
+  document.querySelector(".underpopular2").style.display = "block";
+});
+//-------------------------------------------------------------------------------
